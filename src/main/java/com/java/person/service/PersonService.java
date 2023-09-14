466244c529665706a62b700543138af5b610151f
SruthiJalagam <sruthijalagam14@gmail.com>
2023-09-14 17:25:40 -0400

    person controller

---
+++ src/main/java/com/java/person/service/PersonService.java
@@ -0,0 +1,27 @@
+package com.java.person.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.java.person.bean.Person;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PersonService {
+
+    public String processInput(String input) throws JsonProcessingException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        ArrayList<Person> personArrayList = objectMapper.readValue(input,
+                TypeFactory.defaultInstance().constructCollectionType(List.class, Person.class));
+
+        System.out.println("Person object is " + personArrayList.size());
+
+
+        return "";
+    }
+}
